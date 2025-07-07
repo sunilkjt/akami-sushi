d1abc347a0a2d07879389c58e5892f333d907511
sunilkjt <sunil.kjt@gmail.com>
2025-07-07 14:40:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('theme-toggle').addEventListener('click', () => {
   document.body.classList.toggle('dark');
+  document.body.classList.toggle('bg-gray-900');
+  document.body.classList.toggle('text-white');
 });
